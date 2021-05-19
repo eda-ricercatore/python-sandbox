8b5e52212b2a7213a7311e1f7835de2ebe3c92c8
Zhiyang Ong <ongz@acm.org>
2021-05-19 16:48:05 -0500

    Port repo from ricerca-sandbox.

---
+++ time-processing/test_time_operations.py
@@ -0,0 +1,69 @@
+#!/usr/local/bin/python3
+###	#!/Users/zhiyang/anaconda3/bin/python3
+
+import time
+#from datetime import date
+#from datetime import datetime
+#from datetime import date, datetime, tzinfo, timezone
+from datetime import date, datetime, timezone
+#import datetime
+
+#utctimestamp = datetime.utcfromtimestamp()
+utctimestamp = date.fromtimestamp(time.time())
+print("1) current time, date.fromtimestamp(time.time(), is:",utctimestamp,"=")
+#utctimestamp = datetime.utcfromtimestamp(time.time())
+#print("current time is:",utctimestamp,"=")
+utctimestamp = time.time()
+print("2) current time, time.time(), is:",utctimestamp,"=")
+utctimestamp = time.time_ns()
+print("2a) current time, time.time_ns(), is:",utctimestamp,"=")
+#utctimestamp = datetime.utcnow()
+#utctimestamp = datetime.now()
+"""
+utctimestamp = datetime.utcnow().timestamp()
+print("3) current time is:",utctimestamp,"=")
+utctimestamp = datetime.utcnow()
+print("4) current time is:",utctimestamp,"=")
+"""
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+current_timestamp = current_milli_time()
+print("5) current time, int(round(time.time() * 1000)), is:",current_timestamp,"=")
+
+
+"""
+	Conclusion:
+		Use time.time() to measure time in seconds.
+"""
+utctimestamp = time.time()
+print("current time, time.time(), is:",utctimestamp,"=")
+current_date = date.fromtimestamp(utctimestamp)
+print("current date, date.fromtimestamp(time.time()), is:",current_date,"=")
+current_date_time = datetime.fromtimestamp(utctimestamp)
+print("current date, datetime.fromtimestamp(time.time()), is:",current_date_time,"=")
+current_date_time = datetime.utcfromtimestamp(utctimestamp)
+print("current date, datetime.utcfromtimestamp(time.time()), is:",current_date_time,"=")
+#current_date_time = datetime(tzinfo=timezone.utc).timestamp()
+#current_date_time = time.time()
+current_date_time = datetime.now()
+current_date_time = current_date_time.replace(tzinfo=timezone.utc)
+print("current date, datetime.now().replace(tzinfo=timezone.utc), is:",current_date_time,"=")
+#utctimestamp = time.gmtime()
+#print("current time, time.gmtime(), is:",utctimestamp,"=")
+
+"""
+	Remarks/Comments:
+	
+	Unix time (also known as Epoch time,[1][2][3] POSIX time,[4] seconds since the Epoch,[5] or UNIX Epoch time[6]) is a system for describing a point in time.
+		References:
+			Wikipedia contributors, ``Unix time,'' in *Wikipedia, The Free Encyclopedia: Calendaring standards*, Wikimedia Foundation, San Francisco, CA, September 15, 2019. Available online from *Wikipedia, The Free Encyclopedia: Calendaring standards* at: *https://en.wikipedia.org/wiki/Unix_time; last accessed on September 16, 2019.
+			\cite[time — Time access and conversions]{DrakeJr2016b}
+				https://docs.python.org/3/library/time.html#time.time
+
+	Hence, using the time.time() method to obtain the timestamp will
+		provide the timestamp in UTC time zone.
+	
+	Hence, use time.time() to record the timestamp in UTC time zone,
+		and avoid confusion between time zones on projects with
+		geographically distributed contributors/collaborators.
+"""
---
+++ time-processing/timing-measurement-notes.md
@@ -0,0 +1,31 @@
+#	Notes about Timing Measurements
+
+
++ [get_factorial.py](https://github.com/eda-ricercatore/gulyas-scripts/blob/master/sandbox/python/time-processing/get_factorial.py)
+	- Functions to calculate factorials using:
+		* iteration
+		* recursion
++ [experimental data](compare_different_methods_to_measure_elapsed_periods.csv)
+	- Experimental data stored in a [comma-separated values file](https://en.wikipedia.org/wiki/Comma-separated_values) that should be plot with [Matplotlib](https://matplotlib.org/).
++ performance/timing measurements
+	- [performance_measurement.py](https://github.com/eda-ricercatore/gulyas-scripts/blob/master/sandbox/python/time-processing/performance_measurement.py)
+	- Collection of different methods to measure the current time in *Python*.
+	- ***Use this to measure the current time.*** [performance_measurement_1.py](https://github.com/eda-ricercatore/gulyas-scripts/blob/master/sandbox/python/time-processing/performance_measurement_1.py)
+		* Measure current time with: ***timeit.timeit()***
+	- [performance_measurement_2.py](https://github.com/eda-ricercatore/gulyas-scripts/blob/master/sandbox/python/time-processing/performance_measurement_2.py)
+		* Measure current time with: ***time.perf_counter()***
+	- [performance_measurement_3.py](https://github.com/eda-ricercatore/gulyas-scripts/blob/master/sandbox/python/time-processing/performance_measurement_3.py)
+		* Measure current time with: ***time.process_time_ns()***
+	- [test_time_operations.py](https://github.com/eda-ricercatore/gulyas-scripts/blob/master/sandbox/python/time-processing/test_time_operations.py)
+		* Sandbox to test different ways of measuring the current time.
+
+
+
+
+
+
+
+
+##	To-Do List
+
+Merge code into my BDA code base. Port it to Google Colab projects.
