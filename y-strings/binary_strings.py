bf1e7070bca21cb273f0d943b18bf7720f0ab326
Zhiyang Ong <ongz@acm.org>
2021-05-19 16:48:20 -0500

    Port repo from ricerca-sandbox.

---
+++ y-strings/binary_strings.py
@@ -0,0 +1,291 @@
+#!/Users/zhiyang/anaconda3/bin/python3
+
+
+###############################################################
+"""
+	Import modules from The Python Standard Library.
+	sys			Get access to any command-line arguments.
+"""
+#import sys
+import numpy as np
+
+
+def binary_string_operations_1():
+	a = [int(i) for i in np.binary_repr(0b0111, 4)]
+	print("a is:",a,".")
+	print("isinstance(a, str) is:",isinstance(a, list),"=")
+	b = "This is "
+	c = "a good way to benchmark circuits."
+	d = b+c
+	print("d is:",d,"=")
+	e = "This is a good way to benchmark circuits."
+	if e.find("benchmark"):
+		print("Found benchmark.")
+	else:
+		print("Benchmark is not found.")
+
+
+	bin_str = bin(83)
+	if isinstance(bin_str, str):
+		print("bin_str is a string:",bin_str,"=")
+	else:
+		print("bin_str is NOT a string!!!")
+	f = "{0:b}".format(7)
+	print("7 as a binary string:",f,"=")
+	print("isinstance(f, str) is:",isinstance(f, str),"=")
+	g = "{0:8b}".format(7)
+	print("7 as an 8-bit binary string:",g,"=")
+	print("isinstance(g, str) is:",isinstance(g, str),"=")
+	print("=	0-value bits are represented by character spaces, rather than 0-value bits.")
+	h = "{0:4b}".format(7)
+	print("7 as a 4-bit binary string:",h,"=")
+	print("isinstance(h, str) is:",isinstance(h, str),"=")
+	try:
+		bin_str = bin(7)
+		j = [int(i) for i in np.binary_repr(bin_str, 5)]
+		print("7 as a 5-bit binary string:",j,"=")
+	except TypeError:
+		print("'bin()' method turns a number into a (binary) string, which cannot work with method.")
+
+
+
+
+def binary_string_operations_2():
+	"""
+		Reference:
+		+ [noobar2017]
+			- noobar, Answer to "Convert binary to list of digits Python",
+				Stack Exchange Inc., New York, NY, March 14, 2017.
+				Available online from Stack Exchange Inc.: Stack Overflow: Questions at: https://stackoverflow.com/a/42779009/1531728 and https://stackoverflow.com/questions/13081090/convert-binary-to-list-of-digits-python/42779009#42779009;
+					February 18, 2020 was the last accessed date.
+	"""
+	a = [int(i) for i in np.binary_repr(0b011010101010, 12)]
+	print("a is:",a,".")
+	a = [int(i) for i in np.binary_repr(0b011010101010, 15)]
+	print("a is:",a,".")
+	"""
+		If the number of bits is less than the number of bits required
+			to represent the number, the method will still represent
+			the entire number (as a list of 0s and 1s) without any
+			leading zeros (which may be present, depending on the value
+			of the binary number if just enough bits are used to represent
+			the binary number).
+
+		From \cite[SciPy.org: Numpy and Scipy Documentation: NumPy v1.17 Manual:
+		NumPy Reference: Routines: Binary operations -- Output
+		formatting]{Jones2018i}
+		+ Updated NumPy Reference as Release 1.17, July 26, 2019.
+		+ Available online from {SciPy.org}: {Numpy} and {Scipy} Documentation:
+			{NumPy} v1.17 Manual: NumPy Reference: Routines: Binary
+			operations -- Output formatting at: https://docs.scipy.org/doc/numpy/reference/generated/numpy.binary_repr.html;
+			March 2, 2020 was the last accessed date
+		+ If the optional "width" parameter that has been "[d]eprecated since
+			version 1.12.0." is not provided, it will truncate the leading zero(s).
+	"""
+	a = [int(i) for i in np.binary_repr(0b011010101010, 5)]
+	print("a is:",a,".")
+	try:
+		a = [int(i) for i in np.binary_repr("9", 15)]
+		print("a is:",a,".")
+	except TypeError:
+		print("Cannot convert '9' to a list of 0-1 integers.")
+		print("Method cannot turn integer-as-a-string into a list of 0s and 1s.")
+
+
+
+
+
+def binary_string_operations_3():
+	try:
+		a = [int(i) for i in np.binary_repr("1100", 15)]
+		print("a is:",a,".")
+	except TypeError:
+		print("Cannot convert '1100' to a list of 0-1 integers.")
+		print("Method cannot turn binary string into a list of 0s and 1s.")
+	# 1100 (in decimal) = 4+8+64+1024
+	a = [int(i) for i in np.binary_repr(1100, 15)]
+	print("a is:",a,".")
+	print("Converted number 1100 to a list of 0-1 integers.")
+	abb = 0b1100
+	if isinstance(abb, int):
+		print("abb is an integer.")
+	else:
+		print("abb is NOT an integer.")
+	ab = [int(i) for i in np.binary_repr(abb, 15)]
+	print("ab is:",ab,".")
+	print("Converted number 0b1100 to a list of 0-1 integers.")
+
+
+
+
+def binary_string_operations_4():
+	try:
+		bc = int(101011,2)
+		print("= Can't event cast number into 'bc'")
+		if 2 == bc.__index__():
+			print("bc is a binary number.")
+		else:
+			print("bc is NOT a binary number!!!")
+	except TypeError:
+		print("TypeError occurred. Primary/First input argument of int() has to be a string.")
+	# 1+2+8+32 = 43
+	bd = int("101011",2)
+	if 2 == bd.__index__():
+		print("bd is a binary number.")
+	else:
+		print("bd is NOT a binary number!!!")
+		print("bd.__index__() is:",bd.__index__(),"=")
+	be = int("0b101011",2)
+	if 2 == be.__index__():
+		print("be is a binary number.")
+	else:
+		print("be is NOT a binary number!!!")
+		print("be.__index__() is:",be.__index__(),"=")
+	bf = int(101011)
+
+
+
+
+def binary_string_operations_5():
+	try:
+		bg = int("7",2)
+		if 2 == bg.__index__():
+			print("bg is a binary number.")
+		else:
+			print("bg is NOT a binary number!!!")
+			print("bf.__index__() is:",bg.__index__(),"=")
+			print("bg is:",bg,"=")
+	except ValueError:
+		print("ValueError! To express a number as a binary number, it has to be a string of '1's and '0's.")
+
+
+
+def binary_string_operations_6():
+	try:
+		ca = int("7",2)
+		cb = int(101011,2)
+		cc = int("101011",2)
+		print("cc is:",cc,"=")
+	except ValueError:
+		print("ValueError! Because of int('7',2).")
+		try:
+			cd = int(101011,2)
+		except TypeError:
+			print("TypeError occurred. int(101011,2) - 2.")
+			ce = int("101011",2)
+			print("ce is:",ce,"=")
+	except TypeError:
+		print("TypeError occurred. int(101011,2).")
+
+
+
+def binary_string_operations_7():
+	a = [int(i) for i in np.binary_repr(0b0111, 4)]
+	print("a is:",a,".")
+	aa = [int(i) for i in np.binary_repr(0b0111)]
+	print("aa is:",aa,".")
+
+
+
+def binary_string_operations_8():
+	"""
+		Integers cannot start with a zero.
+		try-except block fails to work, since it causes an error with
+			np.binary_repr(), prior to being used in int().
+	"""
+	try:
+		#a = [int(i) for i in np.binary_repr(0111, 5)]
+		#qwerty = np.binary_repr(0111, 5)
+		qwerty = np.binary_repr(111, 5)
+		print("qwerty is:",qwerty,".")
+	except SyntaxError:
+		a = [int(i) for i in np.binary_repr(111, 5)]
+		print("a is:",a,".")
+
+
+
+def binary_string_operations_9():
+	# Integers cannot start with a zero.
+	try:
+		#a = 0836
+		das = 4567
+		print("das is:",das,".")
+	except SyntaxError:
+		a = 836
+		print("a is:",a,".")
+
+
+
+
+
+
+
+def binary_string_operations_10():
+	dd1 = [int(i) for i in np.binary_repr(0b0111, 4)]
+	print("dd1 is:",dd1,".")
+	# Cannot assign a 0-begining integer value to a variable.
+	#dd2 = [int(i) for i in np.binary_repr(0111, 4)]
+	dd2 = [int(i) for i in np.binary_repr(9, 4)]
+	print("dd2 is:",dd2,".")
+	dd3 = [int(i) for i in np.binary_repr(9, 6)]
+	print("dd3 is:",dd3,".")
+	"""
+		np.binary_repr() doesn't work for floating-point numbers.
+	dd4 = [int(i) for i in np.binary_repr(1.25, 6)]
+	"""
+	dd4 = [int(i) for i in np.binary_repr(32, 6)]
+	print("dd4 is:",dd4,".")
+
+
+
+
+def binary_string_operations_11():
+	ee = 0b0111
+	ef = str(ee)
+	print("ef is:",ef,"=")
+	eg = bin(ee)
+	print("eg is:",eg,"=")
+
+
+
+
+
+def binary_string_operations_12():
+	bin_str = bin(13)
+	"""
+		Turn binary string into an integer, in base 2, but stored
+			as an integer in base 10 (decimal number).
+	"""
+	bin_str_as_int = int(bin_str,2)
+	print("bin_str_as_int as:",bin_str_as_int,"=")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################
+# Main method for the program.
+
+#	If this is executed as a Python script,
+if __name__ == "__main__":
+	binary_string_operations_1()
+	binary_string_operations_2()
+	binary_string_operations_3()
+	binary_string_operations_4()
+	binary_string_operations_5()
+	binary_string_operations_6()
+	binary_string_operations_7()
+	binary_string_operations_8()
+	binary_string_operations_9()
+	binary_string_operations_10()
+	binary_string_operations_11()
+	binary_string_operations_12()
---
+++ y-strings/string_processing.py
@@ -0,0 +1,378 @@
+#!/usr/local/bin/python3
+
+"""
+	This is written by Zhiyang Ong to test concepts in string processing.
+
+	Synopsis:
+	Script to process strings.
+
+
+ Revision History:
+ 1) November 13, 2014. Initial working version.
+
+
+
+ 	The MIT License (MIT)
+
+ 	Copyright (c) <2014> <Zhiyang Ong>
+
+ 	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ 	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ 	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"
+"""
+
+
+#	Import packages and functions from the Python Standard Library.
+import string
+import os
+import sys
+
+
+#	============================================================
+
+
+my_str = "UC Berkeley's logic synthesis tool, ABC, is the best logic synthesis and verification tool in the world."
+print(my_str)
+
+my_str = my_str.replace("UC Berkeley", "MIT")
+print(my_str)
+
+filename, file_extension = os.path.splitext('daniela-stefanescu')
+if None == file_extension:
+	print("file_extension is equal to None.")
+else:
+	print("file_extension is equal to something else.")
+
+if not file_extension:
+	print("file_extension is empty.")
+else:
+	print("file_extension is not empty???")
+
+
+base_directory = "/Users/zhiyang/Documents/ricerca/risultati_sperimentali/std-cell-library-characterization"
+a_path = "/Users/zhiyang/Documents/ricerca/risultati_sperimentali/std-cell-library-characterization/2020"
+if a_path.startswith(base_directory):
+	print("=	a_path starts with base_directory.")
+else:
+	print("=	a_path DOES NOT start with base_directory.")
+if "tyriuoipo".startswith(base_directory):
+	print("=	tyriuoipo STARTS WITH base_directory.")
+else:
+	print("=	a_path does not start with base_directory.")
+if "tyriuoipo".startswith("dwedew"):
+	print("=	tyriuoipo STARTS WITH base_directory.")
+else:
+	print("=	a_path does not start with base_directory.")
+
+
+
+
+
+
+
+
+print("\n\n")
+"""
+	The Python method "os.path.splitext()" to split the path
+		for a file into the root of the path (without the
+		file extension suffix) and the file extension.
+
+
+	Unfortunately, by default, it defines the file extension
+		as the string starting from the last period (if it
+		exists) till the end of the string.
+	Hence, file extensions such as ".tar.gz" would not be
+		recognized and detected correctly.
+	If I want to define file extension otherwise, I have
+		to provide an implementation of a Python method
+		complying with the new definition.
+
+	References:
+	+ BibTeX key: DrakeJr2016b
+		- From the section "File and Directory Access",
+			subsection "os.path - Common pathname manipulations".
+		- By definition of the "os.path.splitext ()" (Python)
+			method returns the root of the file's path and
+			the file extension.
+"""
+print ("25-3-2010-5-8-51-9407.txt is being partitioned into the filename without the file extension suffix and the file extension.")
+filename_wo_extn, file_extn = os.path.splitext ("25-3-2010-5-8-51-9407.txt")
+print ("filename_wo_extn is:", filename_wo_extn, "=")
+print("file_extn is:",file_extn,"=")
+print("/path/to/filename.pdf.tar.gz is being partioned into the filename without the file extension suffix and the file extension.")
+filename_wo_extn, file_extn = os.path.splitext("/path/to/filename.pdf.tar.gz")
+print("filename_wo_extn is:",filename_wo_extn,"=")
+print("file_extn is:",file_extn,"=")
+tokens = filename_wo_extn.split("-")
+print("isinstance(tokens, list) is:",isinstance(tokens, list),"=")
+print("isinstance(None, list) is:",isinstance(None, list),"=")
+print("isinstance('dewd323 vrevnoi.123', str) is:",isinstance("dewd323 vrevnoi.123", str),"=")
+print("isinstance(None, str) is:",isinstance(None, str),"=")
+print("isinstance(32.234, str) is:",isinstance(32.234, str),"=")
+print("\n\n")
+
+
+
+
+months_of_the_year = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+print("months_of_the_year[2] is:",months_of_the_year[2-1],"=")
+#print("months_of_the_year[02] is:",months_of_the_year[int(02)],"=")
+print("months_of_the_year[10] is:",months_of_the_year[10-1],"=")
+
+
+
+a_bin_str = "0b0010100110"
+b_bin_str = a_bin_str[2:]
+print("b_bin_str is:",b_bin_str,"=")
+
+
+c_string = "I love fried plantains."
+for char in c_string:
+	print("current character is:", char, "=")
+	if ('a' == char) or ('n' == char):
+		print("=	Found 'a' or 'n'.")
+print("======================================================")
+empty_string = ""
+# Is the string empty?
+if not empty_string:
+	print("+	The string is empty.")
+else:
+	print("+	The string is NOT empty!!!")
+not_empty_string = "String is not empty."
+# Is the string empty?
+if not_empty_string:
+	print("+	The string is NOT empty.")
+else:
+	print("+	The string is EMPTY!!!")
+
+print("======================================================")
+
+my_string = ""
+
+if not my_string:
+	print("my_string is empty:",my_string,"=")
+else:
+	print("The string is:",my_string,"=")
+
+my_string = "Hello World"
+
+if not my_string:
+	print("my_string is empty:",my_string,"=")
+else:
+	print("The string is:",my_string,"=")
+
+print("= length of input arguments to the program:",len(sys.argv),"=")
+if 1 < len(sys.argv):
+	print("the first argument is:",sys.argv[1],"=")
+
+print("======================================================")
+
+a = 45.5
+b = 98
+c = 0.23
+d = 34
+prompt = " a is:{}= and b is:{}= and c is:{}= and d is:{}="
+print(prompt .format(a,b,c,d))
+
+
+print("======================================================")
+
+"""
+	Methods to replace/delete a substring.
+
+	References:
+	+ [Garg2019]
+		- https://www.geeksforgeeks.org/python-remove-the-given-substring-from-end-of-string/ 
+		- Python | Remove the given substring from end of string
+		- 07-06-2019   
+		- garg_ak0109
+		- Akshat Garg
+"""
+
+
+
+my_string = "This is an substring that I want to eliminate."
+print("= my_string is:",my_string,"=")
+my_substring = "substring that I "
+print("= my_substring is:",my_substring,"=")
+
+print("= Method 2 from [Garg2019].")
+import re
+result_1 = re.sub(my_substring, "", my_string)
+print("= result_1 is:",result_1,"=")
+
+print("= Method 3 from [Garg2019].")
+result_2 = my_string.replace(my_substring, "")
+print("= result_2 is:",result_2,"=")
+
+
+my_substring = "4568798yu9cwever32r"
+print("= Try deleting non-existent substring.")
+result_3 = my_string.replace(my_substring, "")
+print("= result_3 is:",result_3,"=")
+if result_3 == my_string:
+	print("= result_3 and my_string are the same.")
+else:
+	print("= result_3 and my_string are different.")
+
+print("======================================================")
+
+"""
+	Methods to determine if a substring exists in a string.
+
+	References:
+	+ [Stopak20XY]
+		- https://stackabuse.com/python-check-if-string-contains-substring/
+		- Python: Check if String Contains Substring
+		- Jacob Stopak
+		- No date
+	+ [Edpresso Editor 2020]
+		- https://www.educative.io/edpresso/how-to-check-if-python-string-contains-another-string   
+		- How to check if Python string contains another string
+		- August 12, 2020.
+		- Educative, Inc.: Bellevue, WA.
+	+ [Striver2000]
+		- https://www.geeksforgeeks.org/python-check-substring-present-given-string/
+		- Python | Check if a Substring is Present in a Given String
+		- Last Updated: 18-05-2020
+	+ [Kumar20XY]
+		- https://www.askpython.com/python/string/check-string-contains-substring-python 
+		- How to Check if a String contains a Substring in Python?
+		- Python String
+		- Python Programming
+		- AskPython.com
+		- pankaj.0323@gmail.com
+		- Pankaj Kumar, "How to Check if a String contains a Substring in Python?," from AskPython.com: Python Programming: Python String, JournalDev IT Services Private Limited, Bengaluru, Karnataka, India. Available online from JournalDev IT Services Private Limited: AskPython.com: Python Programming: Python String at: https://www.askpython.com/python/string/check-string-contains-substring-python; August 24, 2020 was the last accessed date.
+
+
+
+   
+August 12, 2020
+
+	Notes:
+	+ "Lists are iterables, and the in method acts on iterables, not just strings."
+		- From: https://stackoverflow.com/a/43687082
+		- firelynx (author of quote)
+		- Apr 28 '17 at 18:52
+		- Community at: Jun 20 at 9:12
+		- firelynx and Community, Answer to "Does Python have a string 'contains' substring method?", Stack Exchange Inc., New York, NY, June 20, 2020. Available online from Stack Exchange Inc.: Stack Overflow: Questions at: https://stackoverflow.com/a/43687082 and https://stackoverflow.com/questions/3437059/does-python-have-a-string-contains-substring-method/43687082#43687082; March 7, 2020 was the last accessed date.
+"""
+
+print("= Method 1 from [Stopak20XY]. Method 2 from [Edpresso Editor 2020]")
+if my_substring in my_string:
+	print("my_string contains my_substring.")
+else:
+	print("my_string does NOT contain my_substring!!!")
+
+print("= Modify my_string and my_substring.")
+my_string = "drtifyougiphojp tofyguhci eiu ucvwek32 89r 	jfkr o"
+print("= my_string is:",my_string,"=")
+my_substring = "rtftyguihojk"
+print("= my_substring is:",my_substring,"=")
+if my_substring not in my_string:
+	print("my_string does not contain my_substring.")
+else:
+	print("my_string CONTAINS my_substring!!!")
+if my_substring in my_string:
+	print("alt: my_string CONTAINS my_substring!!!")
+else:
+	print("alt: my_string does not contain my_substring.")
+
+
+print("")
+print("= Method 3 from [Stopak20XY]. Method 1 from [Edpresso Editor 2020].")
+my_string = "This is an substring that I want to eliminate."
+my_substring = "substring that I "
+if -1 != my_string.find(my_substring):
+	print("my_string contains my_substring.")
+else:
+	print("my_string does NOT contain my_substring!!!")
+print("= Modify my_string and my_substring.")
+my_string = "drtifyougiphojp tofyguhci eiu ucvwek32 89r 	jfkr o"
+print("= my_string is:",my_string,"=")
+my_substring = "rtftyguihojk"
+print("= my_substring is:",my_substring,"=")
+if -1 != my_string.find(my_substring):
+	print("alt: my_string CONTAINS my_substring!!!")
+else:
+	print("alt: my_string does not contain my_substring.")
+
+
+
+print("")
+print("= Method 4 from [Stopak20XY].")
+from re import search
+my_string = "This is an substring that I want to eliminate."
+my_substring = "substring that I "
+if search(my_substring, my_string):
+	print("my_string contains my_substring.")
+else:
+	print("my_string does NOT contain my_substring!!!")
+print("= Modify my_string and my_substring.")
+my_string = "drtifyougiphojp tofyguhci eiu ucvwek32 89r 	jfkr o"
+print("= my_string is:",my_string,"=")
+my_substring = "rtftyguihojk"
+print("= my_substring is:",my_substring,"=")
+if search(my_substring, my_string):
+	print("alt: my_string CONTAINS my_substring!!!")
+else:
+	print("alt: my_string does not contain my_substring.")
+
+
+ 
+print("")
+print("= Method 3 from [Edpresso Editor 2020].")
+my_string = "This is an substring that I want to eliminate."
+my_substring = "substring that I "
+if 0 != my_string.count(my_substring):
+	print("my_string contains my_substring.")
+else:
+	print("my_string does NOT contain my_substring!!!")
+print("= Modify my_string and my_substring.")
+my_string = "drtifyougiphojp tofyguhci eiu ucvwek32 89r 	jfkr o"
+print("= my_string is:",my_string,"=")
+my_substring = "rtftyguihojk"
+print("= my_substring is:",my_substring,"=")
+if 0 != my_string.count(my_substring):
+	print("alt: my_string CONTAINS my_substring!!!")
+else:
+	print("alt: my_string does not contain my_substring.")
+
+
+
+print("")
+print("= Method 5 from [Kumar20XY].")
+import operator
+my_string = "This is an substring that I want to eliminate."
+my_substring = "substring that I "
+if operator.contains(my_string,my_substring):
+	print("my_string contains my_substring.")
+else:
+	print("my_string does NOT contain my_substring!!!")
+print("= Modify my_string and my_substring.")
+my_string = "drtifyougiphojp tofyguhci eiu ucvwek32 89r 	jfkr o"
+print("= my_string is:",my_string,"=")
+my_substring = "rtftyguihojk"
+print("= my_substring is:",my_substring,"=")
+if operator.contains(my_string,my_substring):
+	print("alt: my_string CONTAINS my_substring!!!")
+else:
+	print("alt: my_string does not contain my_substring.")
+
+print("======================================================")
+my_string = "This is an substring that I want to eliminate from my sentence."
+set_of_substrings = {"substring that I ", "to eliminate from", "non-existent"}
+"""
+	Note that the order of elements in the set that selected for
+		processing varies between execution of this Python script.
+"""
+for x in set_of_substrings:
+	print("=	substring:",x)
+	if x in my_string:
+		print("substring exists in my_string.")
+	else:
+		print("substring is not found in my_string.")
