b97accb7b0020e9ffd5f4537c5493c7237bd3b5f
Zhiyang Ong <ongz@acm.org>
2021-11-14 16:07:22 -0600

    Try methods to open multiple file streams concurrently.

--- file-io-and-other-io/multiple_file_input_streams.py
+++ file-io-and-other-io/multiple_file_input_streams.py
@@ -67,6 +67,14 @@ import warnings
 import re
 import datetime
 import time
+# For method #2 from [Marnach2019].
+from contextlib import ExitStack
+
+
+
+
+
+
 
 
 
@@ -82,11 +90,11 @@ import time
 	We can also modify this for creating multiple output file streams
 		concurrently.
 
-	References:
-	+ [Rands2021]
-		- Chris Rands, Answer to `How can I open multiple files using ``with open'' in {Python}?', Stack Exchange, Inc., New York, NY, February 26, 2021. Available online from Stack Exchange Inc.: Stack Overflow: Questions at: https://stackoverflow.com/a/66388647/1531728 and https://stackoverflow.com/questions/4617034/how-can-i-open-multiple-files-using-with-open-in-python/66388647#66388647; November 13, 2021 was the last accessed date.
+	Reference:
+	+ [Marnach2019]
+		- Sven Marnach, Answer to `How can I open multiple files using ``with open'' in {Python}?', Stack Exchange, Inc., New York, NY, February 10, 2019. Available online from Stack Exchange Inc.: Stack Overflow: Questions at: https://stackoverflow.com/a/4617069/1531728 and https://stackoverflow.com/questions/4617034/how-can-i-open-multiple-files-using-with-open-in-python/4617069#4617069; November 13, 2021 was the last accessed date.
 """
-#with (open("./test-cases/input-files/ip-file-1.md", "r") as ip_file_1, open("./test-cases/input-files/ip-file-2.md", "r") as ip_file_2, open("./test-cases/input-files/ip-file-3.md", "r") as ip_file_3):
+print("= Try method #1 from [Marnach2019].")
 with open("./test-cases/input-files/ip-file-1.md", "r") as ip_file_1, open("./test-cases/input-files/ip-file-2.md", "r") as ip_file_2, open("./test-cases/input-files/ip-file-3.md", "r") as ip_file_3:
 	"""
 		Use file_object.readline() method to read the next line from a file
@@ -119,6 +127,106 @@ ip_file_1.readline()
 ip_file_2.readline()
 ip_file_3.readline()
 """
+
+
+
+
+
+"""
+	Reference:
+	+ [Salgado2021]
+		- Pablo Galindo Salgado, "What's New In Python 3.10: New Features: Parenthesized context managers", Python Software Foundation, Beaverton, OR, November 14, 2021. Available online from Python: Python 3.10.0 documentation: What's New In Python as Release 3.10.0 at: https://docs.python.org/3.10/whatsnew/3.10.html#parenthesized-context-managers; November 14, 2021 was the last accessed date.
+"""
+print("= Try method from [Salgado2021].")
+#with (open("./test-cases/input-files/ip-file-1.md", "r") as ip_file_1, open("./test-cases/input-files/ip-file-2.md", "r") as ip_file_2, open("./test-cases/input-files/ip-file-3.md", "r") as ip_file_3):
+with (
+	open("./test-cases/input-files/ip-file-1.md", "r") as ip_file_1,
+	open("./test-cases/input-files/ip-file-2.md", "r") as ip_file_2,
+	open("./test-cases/input-files/ip-file-3.md", "r") as ip_file_3
+	):
+	"""
+		Use file_object.readline() method to read the next line from a file
+			[Brandl2017a, in \S7 Input and Output: \S 7.2. Reading and Writing Files: \S7.2.1 Methods of File Objects].
+
+		Read line #1.
+	"""
+	print(ip_file_1.readline())
+	print(ip_file_2.readline())
+	print(ip_file_3.readline())
+	# Read line #2.
+	ip_file_1.readline()
+	ip_file_2.readline()
+	ip_file_3.readline()
+	# Read line #3.
+	print(ip_file_1.readline())
+	print(ip_file_2.readline())
+	print(ip_file_3.readline())
+	# Read line #3.
+	ip_file_1.readline()
+	ip_file_2.readline()
+	ip_file_3.readline()
+	# Read line #3.
+	print(ip_file_1.readline())
+	print(ip_file_2.readline())
+	print(ip_file_3.readline())
+
+
+
+
+
+
+
+
+
+
+
+"""
+	Reference:
+	+ [Marnach2019]
+		- Sven Marnach, Answer to `How can I open multiple files using ``with open'' in {Python}?', Stack Exchange, Inc., New York, NY, February 10, 2019. Available online from Stack Exchange Inc.: Stack Overflow: Questions at: https://stackoverflow.com/a/4617069/1531728 and https://stackoverflow.com/questions/4617034/how-can-i-open-multiple-files-using-with-open-in-python/4617069#4617069; November 13, 2021 was the last accessed date.
+"""
+print("= Try method #2 from [Marnach2019].")
+filenames = ["./test-cases/input-files/ip-file-1.md", "./test-cases/input-files/ip-file-2.md", "./test-cases/input-files/ip-file-3.md"]
+with ExitStack() as stack:
+	files = [stack.enter_context(open(fname)) for fname in filenames]
+	"""
+		Use file_object.readline() method to read the next line from a file
+			[Brandl2017a, in \S7 Input and Output: \S 7.2. Reading and Writing Files: \S7.2.1 Methods of File Objects].
+
+		Read line #1.
+	"""
+	print(files[0].readline())
+	print(files[1].readline())
+	print(files[2].readline())
+	# Read line #2.
+	files[0].readline()
+	files[1].readline()
+	files[2].readline()
+	# Read line #3.
+	print(files[0].readline())
+	print(files[1].readline())
+	print(files[2].readline())
+	# Read line #3.
+	files[0].readline()
+	files[1].readline()
+	files[2].readline()
+	# Read line #3.
+	print(files[0].readline())
+	print(files[1].readline())
+	print(files[2].readline())
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
