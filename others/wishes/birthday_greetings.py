8e468aa1847097046ebaae0c1677fccb7c1f4df7
Zhiyang Ong <zhiyang.ong@tamu.edu>
2022-08-09 23:15:09 -0500

    Code snippet to wish people, "Happy Birthday!"

---
+++ others/wishes/birthday_greetings.py
@@ -0,0 +1,18 @@
+"""
+	Python script to wish people, "Happy Birthday," repeatedly
+		whenever it is their birthday.
+	If they run the program indefinitely on their birthday
+		(in 2025, 2030, or 2100), it will keep wishing them,
+		"Happy Birthday!"
+	
+	This Python code snippet can be run at:
+		https://replit.com/languages/python3
+"""
+#	Import Python module from Python Standard Library for operations about dates.
+import datetime
+#	Call function of the aforementioned module to determine the current date.
+now = datetime.datetime.now()
+#	While it is your birthday, ...
+while "08-10" == now.strftime('%m-%d'):
+	# Keep wishing you, "Happy Birthday!"
+	print('Happy Birthday, Dr. BLAH!')
