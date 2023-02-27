7f35c88053ec2c61df3e380bfd45a09a6d6c98e1
Zhiyang Ong <ongz@acm.org>
2023-02-27 11:44:47 -0600

    Example of enumerating empty list and dictionary.

---
+++ i-data-structures/w_enumerate_empty_data_struc.py
@@ -0,0 +1,76 @@
+#!/Users/zhiyang/anaconda3/bin/python3
+
+"""
+	This Python script is written by Zhiyang Ong to test the enumeration
+		of empty data structures.
+
+
+
+	References:
+	+ BLAH...
+"""
+
+
+
+
+__author__ = 'Zhiyang Ong'
+__version__ = '1.0'
+__date__ = 'January 17, 2020'
+
+#	The MIT License (MIT)
+
+#	Copyright (c) <2020> <Zhiyang Ong>
+
+#	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+#	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"	Che cosa significa?
+
+
+
+
+
+
+
+
+
+
+
+###############################################################
+#	Import packages and modules from the Python Standard Library.
+import math
+
+
+###############################################################
+#	Import Custom Python Packages and Modules
+
+
+###############################################################
+#	Import packages and modules Python libraries
+
+import numpy as np
+
+
+
+
+
+# Create an empty list
+empty_list = []
+print("Enumerate the empty list.")
+for x in empty_list:
+	print("x is:",x,"=")
+
+
+# Create an empty dictionary
+empty_dict = {}
+print("Enumerate the empty dictionary.")
+for x in empty_dict:
+	print("x is:",x,"=")
+
+
+print("End of script.")
+print("")
+print("")
