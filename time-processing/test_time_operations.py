5137cc40c4abe1966133e204f6821c18e0ed4a55
Zhiyang Ong <ongz@acm.org>
2024-07-05 23:37:21 -0500

    Tried some techniques for getting the date-timestamp.

--- time-processing/test_time_operations.py
+++ time-processing/test_time_operations.py
@@ -10,7 +10,7 @@ from datetime import date, datetime, timezone
 
 #utctimestamp = datetime.utcfromtimestamp()
 utctimestamp = date.fromtimestamp(time.time())
-print("1) current time, date.fromtimestamp(time.time(), is:",utctimestamp,"=")
+print("1) current time, date.fromtimestamp(time.time()), is:",utctimestamp,"=")
 #utctimestamp = datetime.utcfromtimestamp(time.time())
 #print("current time is:",utctimestamp,"=")
 utctimestamp = time.time()
@@ -27,8 +27,9 @@ print("4) current time is:",utctimestamp,"=")
 """
 
 current_milli_time = lambda: int(round(time.time() * 1000))
-current_timestamp = current_milli_time()
-print("5) current time, int(round(time.time() * 1000)), is:",current_timestamp,"=")
+print("4a) current time, lambda: int(round(time.time() * 1000)), is:",current_milli_time,"=")
+current_milli_time = int(round(time.time() * 1000))
+print("5) current time, int(round(time.time() * 1000)), is:",current_milli_time,"=")
 
 
 """
@@ -67,3 +68,28 @@ print("current date, datetime.now().replace(tzinfo=timezone.utc), is:",current_d
 		and avoid confusion between time zones on projects with
 		geographically distributed contributors/collaborators.
 """
+print("============================================================")
+"""
+	From [DrakeJr2023i, The Python Standard Library: Generic Operating System Services: time -- Time access and conversions: Functions]
+"""
+# Has microsecond feature.
+#current_time = time.strptime("%B-%d-%Y-%H-%M-%S-%f")
+# Has no microsecond feature.
+#current_time = time.strptime("%B-%d-%Y-%H-%M-%S")
+# Has no second feature.
+#current_time = time.strptime("%B-%d-%Y-%H-%M", gmtime())
+current_time = time.asctime()
+print("	time.asctime() is:",current_time,"=")
+current_time = time.ctime()
+print("	time.ctime() is:",current_time,"=")
+current_time = time.localtime()
+print("	time.localtime() is:",current_time,"=")
+
+
+month_number_to_text = {1:"january", 2:"february", 3:"march", 4:"april", 5:"may", 6:"june", 7:"july", 8:"august", 9:"september", 10:"october", 11:"november", 12:"december"}
+print("month_number_to_text is:",month_number_to_text,"=")
+print("month_number_to_text[current_time.tm_mon] is:",month_number_to_text[current_time.tm_mon],"=")
+date_timestamp = month_number_to_text[current_time.tm_mon] + "-" + str(current_time.tm_mday) + "-" + str(current_time.tm_year) + "-" + str(current_time.tm_hour) + "-" + str(current_time.tm_min)
+print("	processed date-timestamp is:", date_timestamp, "=")
+current_time = time.gmtime()
+print("	time.gmtime() is:",current_time,"=")
