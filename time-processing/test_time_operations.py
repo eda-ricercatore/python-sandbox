214b31f12be03a058e81e263b37cb3399372bbdc
Zhiyang Ong <ongz@acm.org>
2024-07-06 14:12:23 -0500

    Add comments to script for testing methods to obtain date-timestamps and timestamps.

--- time-processing/test_time_operations.py
+++ time-processing/test_time_operations.py
@@ -1,6 +1,69 @@
 #!/usr/local/bin/python3
 ###	#!/Users/zhiyang/anaconda3/bin/python3
 
+"""
+	This is written by Zhiyang Ong to test operations to get timestamps
+		and date-timestamps.
+
+
+
+	Synopsis: command name and [argument(s)]
+	./test_time_operations.py
+
+	
+
+	
+
+
+	Notes/Assumptions:
+	Since I would be categorizing and storing the experimental results
+		based on the year and month, the filename containg experimental
+		results would be named in the DD-MM-YY-HH-MM-SS-uS format so that
+		I can quickly find the file of a given build (or experimental
+		run) as I read the filename from left to right.
+
+
+
+	References:
+	[SaltyCrane2014]
+		Eliot "Salty Crane", "How to get the current date and time in Python," from Salty Crane Blog, June 26, 2008. Available online from Salty Crane Blog at: https://www.saltycrane.com/blog/2008/06/how-to-get-current-date-and-time-in/; modified on October 22, 2014; self-published; August 31, 2018 was the last accessed date
+
+	[DrakeJr2016b, datetime module, Section 8.1.4 datetime Objects, now() function]
+
+	[DrakeJr2023i, The Python Standard Library: Generic Operating System Services: time -- Time access and conversions: Functions]
+		https://docs.python.org/3/library/time.html#time.strptime
+		https://docs.python.org/3/library/time.html#time.strftime
+
+
+	Revision History:
+	April 17, 2017			Version 0.1, initial build.
+"""
+
+
+#	The MIT License (MIT)
+
+#	Copyright (c) <2014-2017> <Zhiyang Ong>
+
+#	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+#	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; print " "; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d "\n" | tr -d 'ir' | tr y "\n"
+#	Che cosa significa?
+
+#	==========================================================
+
+__author__ = 'Zhiyang Ong'
+__version__ = '0.1'
+__date__ = 'Apr 17, 2017'
+
+###############################################################
+
+
+#	Import modules from The Python Standard Library.
+
 import time
 #from datetime import date
 #from datetime import datetime
